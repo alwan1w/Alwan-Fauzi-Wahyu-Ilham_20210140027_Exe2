19c886ad3e6a1b2df8303b55b673731078f42770
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-11-02 16:47:01 +0700

    create method

--- exe2/Program.cs
+++ exe2/Program.cs
@@ -11,8 +11,23 @@ namespace exe2
         private int[] arr = new int[5];
         private int cmp_count = 0; 
         private int mov_count = 0; 
-
-        
         private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
+        }
     }
 }
