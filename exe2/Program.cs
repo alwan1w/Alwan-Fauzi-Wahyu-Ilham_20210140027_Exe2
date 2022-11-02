e628e209d4797651547faf7962b5b22245047ef6
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-11-02 16:48:08 +0700

    dapatkan array elemen

--- exe2/Program.cs
+++ exe2/Program.cs
@@ -28,6 +28,14 @@ namespace exe2
             Console.WriteLine("\n==================");
             Console.WriteLine("Enter Array Elements");
             Console.WriteLine("\n==================");
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
     }
 }
